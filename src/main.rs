78ec4aed9cc2ec21e73b930751b33b08ce5e819e
Fran Guijarro <franleplant@gmail.com>
2016-11-27 21:44:45 -0300

    minimal working version with iron, it lacks of a rendering engine though

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,88 @@
-#[macro_use] extern crate nickel;
-extern crate chrono;
+//#[macro_use] extern crate nickel;
+//extern crate chrono;
 
 
-use std::env;
-use std::time::Duration;
-use std::collections::HashMap;
-use nickel::{Nickel, Options, Mountable, HttpRouter, StaticFilesHandler};
+//use std::env;
+//use std::time::Duration;
+//use std::collections::HashMap;
+//use nickel::{Nickel, Options, Mountable, HttpRouter, StaticFilesHandler};
 
-use chrono::{DateTime, UTC};
+//use chrono::{DateTime, UTC};
+
+//fn main() {
+    //let mut server = Nickel::new();
+    //server.options = Options::default().thread_count(Some(30));
+
+    //// TODO
+    //println!("{:?}", env::var("ENV"));
+
+    //server.utilize(middleware! { |request|
+        //let time: DateTime<UTC> = UTC::now();
+        //let ref method = request.origin.method;
+        //let ref uri = request.origin.uri;
+        //let ref headers = request.origin.headers;
+        //let ref remote_addr = request.origin.remote_addr;
+        //println!("{} LOG: {} {} \n{}from: {} \n", time, method, uri, headers, remote_addr);
+    //});
+
+    //server.mount("/public/", StaticFilesHandler::new("public/"));
+
+    //server.get("/", middleware! { |_, response|
+        //let mut data = HashMap::new();
+        //data.insert("name", "user");
+        //return response.render("view/index.tpl", &data);
+    //});
+
+
+    //server.keep_alive_timeout(Some(Duration::from_secs(1)));
+    //server.listen("0.0.0.0:8000").unwrap();
+//}
+
+extern crate iron;
+extern crate router;
+extern crate staticfile;
+extern crate mount;
+extern crate logger;
+extern crate env_logger;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+use staticfile::Static;
+use mount::Mount;
+use logger::Logger;
+use logger::format::Format;
+
+use std::path::Path;
+
+
+static FORMAT: &'static str =
+    "{request-time} {method} {uri}, Status: {status}, Duration: {response-time}";
+
+fn handler(req: &mut Request) -> IronResult<Response> {
+    let ref query = req.extensions.get::<Router>().unwrap().find("query").unwrap_or("/");
+    Ok(Response::with((status::Ok, "fuck you")))
+}
 
 fn main() {
-    let mut server = Nickel::new();
-    server.options = Options::default().thread_count(Some(30));
+    env_logger::init().unwrap();
+    let format = Format::new(FORMAT);
 
-    // TODO
-    println!("{:?}", env::var("ENV"));
+    let mut router = Router::new();
 
-    server.utilize(middleware! { |request|
-        let time: DateTime<UTC> = UTC::now();
-        let ref method = request.origin.method;
-        let ref uri = request.origin.uri;
-        let ref headers = request.origin.headers;
-        let ref remote_addr = request.origin.remote_addr;
-        println!("{} LOG: {} {} \n{}from: {} \n", time, method, uri, headers, remote_addr);
-    });
+    router.get("/", handler, "index");
 
-    server.mount("/public/", StaticFilesHandler::new("public/"));
+    let mut mount = Mount::new();
+    mount.mount("/public", Static::new(Path::new("public")));
+    mount.mount("/", router);
 
-    server.get("/", middleware! { |_, response|
-        let mut data = HashMap::new();
-        data.insert("name", "user");
-        return response.render("view/index.tpl", &data);
-    });
+    let (logger_before, logger_after) = Logger::new(Some(format.unwrap()));
+    let mut chain = Chain::new(mount);
+    chain.link_before(logger_before);
+    chain.link_after(logger_after);
 
-
-    server.keep_alive_timeout(Some(Duration::from_secs(1)));
-    server.listen("0.0.0.0:8000").unwrap();
+    match Iron::new(chain).http("0.0.0.0:8000") {
+        Result::Ok(listening) => println!("{:?}", listening),
+        Result::Err(err) => panic!("Error {:?}", err),
+    }
 }
