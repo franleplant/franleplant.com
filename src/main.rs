d8e5f82161e160e24eda17fcc8d702bd3681c622
Fran Guijarro <franleplant@gmail.com>
2016-11-27 22:23:34 -0300

    env vars

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,20 @@ use nickel::{Nickel, Options, Mountable, HttpRouter, StaticFilesHandler};
 use chrono::{DateTime, UTC};
 
 fn main() {
+    let port = match env::var("PORT") {
+        Ok(port) => port,
+        Err(_) => String::from("8000"),
+    };
+
+    let ip = match env::var("IP") {
+        Ok(ip) => ip,
+        Err(_) => String::from("0.0.0.0"),
+    };
+
     let mut server = Nickel::new();
     server.options = Options::default().thread_count(Some(30));
 
-    // TODO
-    println!("{:?}", env::var("ENV"));
+
 
     server.utilize(middleware! { |request|
         let time: DateTime<UTC> = UTC::now();
@@ -35,5 +44,7 @@ fn main() {
 
 
     server.keep_alive_timeout(Some(Duration::from_secs(1)));
-    server.listen("0.0.0.0:8000").unwrap();
+
+    let address: &str = &*format!("{}:{}", ip, port);
+    server.listen(address).unwrap();
 }
