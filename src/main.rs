4faf81f6bb8da653d125bd697ce8276c37a3aac6
Fran Guijarro <franleplant@gmail.com>
2016-11-29 21:42:41 -0300

    env vars

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,54 @@ use nickel::{Nickel, Options, Mountable, HttpRouter, StaticFilesHandler};
 
 use chrono::{DateTime, UTC};
 
+
+struct Config {
+    ip: String,
+    port: u16,
+    thread_count: usize,
+    thread_keepalive: u64,
+}
+
+impl Config {
+    fn new() -> Config {
+        let ip: String = match env::var("IP") {
+            Ok(ip) => ip,
+            Err(_) => "0.0.0.0".to_string(),
+        };
+
+        let port: u16 = match env::var("PORT") {
+            Ok(port) => port.parse().expect("PORT must be a number"),
+            Err(_) => 8000u16,
+        };
+
+        let thread_count: usize = match env::var("THREAD_COUNT") {
+            Ok(count) => count.parse().expect("THREAD_COUNT must be a number"),
+            Err(_) => 30usize,
+        };
+
+        let thread_keepalive: u64 = match env::var("THREAD_KEEPALIVE") {
+            Ok(keepalive) => keepalive.parse().expect("THREAD_KEEPALIVE must be a number"),
+            Err(_) => 1u64,
+        };
+
+
+        Config {
+            ip: ip,
+            port: port,
+            thread_count: thread_count,
+            thread_keepalive: thread_keepalive,
+        }
+    }
+}
+
+
 // TODO env config
-// TODO: better logging
 fn main() {
+    let config = Config::new();
     let mut server = Nickel::new();
     server.options = Options::default().thread_count(Some(30));
 
-    // TODO
-    println!("{:?}", env::var("ENV"));
+
 
     server.utilize(middleware! { |request|
         let time: DateTime<UTC> = UTC::now();
